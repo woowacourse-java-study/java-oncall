ce20e0d9304623bd5db727834469c84a69d2f700
023-dev <0_2_3@naver.com>
2024-11-17 12:39:38 +0900

    feat: setup project

---
+++ src/main/java/oncall/Application.java
@@ -0,0 +1,13 @@
+package oncall;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+    }
+}
+
+public record class mainDto {
+    private int id;
+    private String name;
+    private int age;
+}
